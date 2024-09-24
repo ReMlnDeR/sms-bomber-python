5b596d4bf06bb67eee20502d6f55584a15777a51
Amir <157102068+ReMlnDeR@users.noreply.github.com>
2024-09-24 23:20:58 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 19577102)) #api id
+API_HSH = os.environ.get("API_HASH", "58df0faedccaefeb08d7466831fde2f3") #api hash
+
+admin = int(os.environ.get("admin", 7157564176)) #آیدی عددی ادمین
